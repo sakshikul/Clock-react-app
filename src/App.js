75ef99b06ac8d169767d05523410bb6395f30f54
Sakshi Kulshrestha <kulsakshi24@gmail.com>
2019-05-05 17:54:56 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Clock from './Clock'
+
+import './App.css';
+
+class App extends React.Component{
+  render(){
+    return(
+      <div >
+        <h1>Hello world!</h1>
+        <Clock/>
+      </div>
+    )
+  }
+}
+export default App;
